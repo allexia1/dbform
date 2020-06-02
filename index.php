970d0281ad41c0314036a05a2eb2c4716d380b08
allexia1 <64495661+allexia1@users.noreply.github.com>
2020-06-02 17:42:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+	<!DOCTYPE html>
+	
+	<html>
+	<head lang="pt-br">
+		<meta charset="utf-8">
+		<title>CRUD</title>
+	</head>
+	<body>
+		<H1>CADASTRAR USUÁRIO </H1>
+		<form method="POST" action="processa.php">
+			<label>Nome:</label>
+			<input type="text" name="nome" placeholder="Digite seu nome"> <br><br>
+			<label>Email</label>
+			<input type="email" name="email" placeholder="Digite seu email"> <br><br>
+
+			<input type="submit" value = "cadastrar">
+		</form>
+
+	</body>
+	</html>
